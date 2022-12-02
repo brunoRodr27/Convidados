c6778c8fb4c3fdc8e4a4120496bfbc2f5b2e7229
Bruno Rodrigues <brunodasilvarodrigues.28@gmail.com>
2022-12-02 20:00:52 -0300

    Ajustando Base de Dados

--- app/src/main/java/com/example/convidados/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/repository/GuestRepository.kt
@@ -1,13 +1,17 @@
 package com.example.convidados.repository
 
-class GuestRepository private constructor(){
+import android.content.Context
+
+class GuestRepository private constructor(context: Context){
+
+    private val guestDataBase = GuestDataBase(context)
 
     companion object {
         private lateinit var repository: GuestRepository
 
-        fun getInstance(): GuestRepository {
+        fun getInstance(context: Context): GuestRepository {
             if (!::repository.isInitialized) {
-                repository = GuestRepository()
+                repository = GuestRepository(context)
             }
             return repository
         }
