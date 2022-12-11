d940ff8540059a65a9f463cd1eee9b223ecd4d9b
Bruno Rodrigues <brunodasilvarodrigues.28@gmail.com>
2022-12-11 20:14:20 -0300

    atualizando dados

--- app/src/main/java/com/example/convidados/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/repository/GuestRepository.kt
@@ -2,6 +2,8 @@ package com.example.convidados.repository
 
 import android.content.ContentValues
 import android.content.Context
+import android.provider.ContactsContract.Data
+import androidx.core.content.contentValuesOf
 import com.example.convidados.constants.DataBaseConstants
 import com.example.convidados.model.GuestModel
 
@@ -35,7 +37,21 @@ class GuestRepository private constructor(context: Context){
         }
     }
 
-    fun update() {
+    fun update(guest: GuestModel): Boolean {
+        return try{
+            val db = guestDataBase.writableDatabase
+            val presence = if (guest.presence) 1 else 0
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
 
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 }
