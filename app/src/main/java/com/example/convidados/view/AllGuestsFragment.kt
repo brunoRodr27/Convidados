cefc5d8e0e47463edd6d3bdfadaeba346bcd8fdf
Bruno Rodrigues <brunodasilvarodrigues.28@gmail.com>
2022-12-15 21:52:09 -0300

    Ajustando listagem de convidados

--- app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
+++ app/src/main/java/com/example/convidados/view/AllGuestsFragment.kt
@@ -18,6 +18,7 @@ class AllGuestsFragment : Fragment() {
     private var _binding: FragmentAllGuestsBinding? = null
     private val binding get() = _binding!!
     private lateinit var viewModel: AllGuestsViewModel
+    private val adapter = GuestsAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, b: Bundle?): View {
         viewModel =
@@ -27,7 +28,7 @@ class AllGuestsFragment : Fragment() {
 
         binding.recyclerAllGuests.layoutManager = LinearLayoutManager(context)
 
-        binding.recyclerAllGuests.adapter = GuestsAdapter()
+        binding.recyclerAllGuests.adapter = adapter
 
         viewModel.getAll()
 
@@ -43,7 +44,7 @@ class AllGuestsFragment : Fragment() {
 
     private fun observe() {
         viewModel.guests.observe(viewLifecycleOwner) {
-
+            adapter.updatedGuests(it)
         }
     }
 }
